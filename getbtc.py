d9a46d833ca910d47893c4890b8ea0aa77cd8cf2
hatunina <y.iwama0807@gmail.com>
2018-04-05 23:38:49 +0900

    make getbtc.py

---
+++ getbtc.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import requests
+import pandas as pd
+from progressbar import ProgressBar
+from datetime import datetime as dt
+import datetime
+
+
+class PythonBitFlyerApp(object):
+
+    def __init__(self, count=500, count_limit=20):
+        # before id
+        self.before_id = 0
+        # bit data size
+        self.count = count
+        # loop size
+        self.count_limit = count_limit
+        # request url
+        self.domain_url = 'https://api.bitflyer.jp'
+        self.execution_history_url = '/v1/getexecutions'
+        self.output_file_name = 'bit_data.csv'
+        # column array
+        self.keys = ['id',
+                     'side',
+                     'price',
+                     'size',
+                     'exec_date',
+                     'buy_child_order_acceptance_id',
+                     'sell_child_order_acceptance_id']
+
+        self.first_time_flag = True
+
+        self.execution_history_params = {'count': self.count,
+                                    'before': self.before_id}
+
+    def run(self):
+
+        arg_date = dt.strptime('2018-04-05 12:00:56', '%Y-%m-%d %H:%M:%S')
+        arg_date = arg_date.replace(second=0)
+
+        while True:
+            start_id = self.search_start_id(arg_date)
+            self.execution_history_params['before'] = start_id
+
+
+        # init ProgressBar
+        p = ProgressBar(0, self.count_limit)
+
+        # init DataFrame
+        df = pd.DataFrame(columns=self.keys)
+
+        for progress_num in range(self.count_limit):
+            # request execution history
+            response = self.execute_api_request(self.execution_history_url, self.execution_history_params)
+            btc_list = response.json()
+
+            last_id = btc_list[-1]['id']
+
+            # update parameters with last id
+            self.execution_history_params['before'] = last_id
+
+            # show execution progress
+            p.update(progress_num)
+
+            tmp_df = pd.read_json(response.text)
+            df = pd.concat([df, tmp_df])
+
+        self.save_result_data(df)
+
+    # execute api request
+    def execute_api_request(self, url, params):
+        request_url = self.domain_url + url
+        return requests.get(request_url, params=params)
+
+    def save_result_data(self, result_df):
+        result_df.to_csv(self.output_file_name, index=False)
+        print('save on {}'.format(self.output_file_name))
+
+    def search_start_id(self, arg_date):
+        # request execution history
+        response = self.execute_api_request(self.execution_history_url, self.execution_history_params)
+
+        search_btc_df = pd.read_json(response.text)
+
+
+        str_date = search_btc_df['exec_date'].iloc[0]
+        tmp_date = str_date.replace('T', ' ')
+        tmp_date = tmp_date.split('.')[0]
+        date = dt.strptime(tmp_date, '%Y-%m-%d %H:%M:%S')
+        date = date.replace(second=0)
+
+        # Adjust ISO format to Japan time
+        date = date + datetime.timedelta(hours=9)
+
+        if date < arg_date:
+            # 引数のdateは今取得したbtcデータよりも未来にある
+            # 初回は本当に未来のdateを指定されているのでエラー
+            # ２回目以降は過去に戻りすぎなのでidを増やす
+            if self.first_time_flag:
+                return 'miss'
+            else:
+                start_id = int(search_btc_df['id'][499]) + 5000
+                self.first_time_flag = False
+                return start_id
+        elif date > arg_date:
+            # 引数のdateは今取得したbtcデータよりも過去にある
+            start_id = int(search_btc_df['id'][499]) - 5000
+            return start_id
+        else:
+            print('stop')
+            pass
+
+
+if __name__ == '__main__':
+    python_bit_flyer_app = PythonBitFlyerApp()
+    python_bit_flyer_app.run()
